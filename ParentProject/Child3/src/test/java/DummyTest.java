ce730e810986a6e5deabb3d50d1e4620bd8cdba3
BorisELazarov <134779329+BorisELazarov@users.noreply.github.com>
2024-07-02 13:48:18 +0300

    Added Task 2.4
    Added dependency management of JUnit to be used in Child 2 and Child 3.

---
+++ ParentProject/Child3/src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void testTry()
+    {
+        Integer number=2;
+        assertTrue(number>0);
+    }
+}
